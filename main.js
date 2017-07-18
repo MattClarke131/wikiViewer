bd47040bf74764880eb80f5e14c136c86705e0ce
Matthew Clarke <mattclarkeruns@gmail.com>
2017-07-18 14:43:27 -0400

    Create functioning javascript CORS request function
    Normally browsers disable cross-domain requests for security reasons.
    Cross-Origin Resource Sharing (CORS) allows cross-domain requests.
    Wikipedia now allows CORS requests if "origin=*" is included in the request URL.

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // Main.js
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // Check if XMLHttpRequest object has "withCredentials" property.
+    // Chrome, Firefox, Opera, and Safari use XMLHTTPRequest
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE uses XDomainRequest
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS is not supported by the browser
+    xhr = null;
+  };
+  return xhr;
+};
+
+function search(searchterm) {
+  var xhr = createCORSRequest('GET', 'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json&search='+searchterm+'&namespace=0&limit=10')
+  if(!xhr) {
+    throw new Error('CORS not supported');
+  };
+  xhr.onload = function() {
+    var responseText = JSON.parse(xhr.responseText);
+    console.log(responseText);
+  };
+  xhr.onerror = function() {
+    console.log("Error in CORS Request!");
+  }
+  xhr.send();
+}
